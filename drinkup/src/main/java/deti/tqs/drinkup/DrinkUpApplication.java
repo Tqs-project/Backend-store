a727f741a5e1cee74986b1e53fd03fe67fc7b3f4
Orlando-pt <orlando.macedo15@ua.pt>
2021-06-14 19:10:45 +0100

    initial commit

---
+++ drinkup/src/main/java/deti/tqs/drinkup/DrinkUpApplication.java
@@ -0,0 +1,13 @@
+package deti.tqs.drinkup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkUpApplication.class, args);
+	}
+
+}
