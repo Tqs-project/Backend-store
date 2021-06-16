2102b207817afd2349c3391ff1b5b51c60839b3a
Rui-FMF <ruifmf2000@gmail.com>
2021-06-17 00:57:00 +0100

    initial controller structure

--- drinkup/src/main/java/deti/tqs/drinkup/controller/OrderController.java
+++ drinkup/src/main/java/deti/tqs/drinkup/controller/OrderController.java
@@ -1,4 +1,23 @@
 package deti.tqs.drinkup.controller;
 
+import deti.tqs.drinkup.service.OrderService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@RequestMapping("/api/order")
 public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    //public methods for endpoints, same as service
+
+
+    //private TokenDto login();
+    //method that will be called in each endpoint to ensure the DrinkUp is authenticated, probably will be moved to an "utils" folder
+
 }
--- drinkup/src/main/java/deti/tqs/drinkup/service/OrderServiceImp.java
+++ drinkup/src/main/java/deti/tqs/drinkup/service/OrderServiceImp.java
@@ -1,4 +1,11 @@
 package deti.tqs.drinkup.service;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Log4j2
+@Service
+@Transactional
 public class OrderServiceImp implements OrderService{
 }
